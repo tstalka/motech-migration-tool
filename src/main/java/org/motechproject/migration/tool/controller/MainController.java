bf9b2ce0b35701500a49d852a546cd08787bbfa6
Tomasz Stalka <tstalka@soldevelo.com>
2015-12-15 15:48:20 +0100

    Fields for database username and password

--- src/main/java/org/motechproject/migration/tool/controller/MainController.java
+++ src/main/java/org/motechproject/migration/tool/controller/MainController.java
@@ -1,4 +1,28 @@
 package org.motechproject.migration.tool.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    protected void migrate(ActionEvent event) {
+    }
+
+    @FXML
+    protected void usernameChanged(Event event) {
+    }
+
+    @FXML
+    protected void passwordChanged(Event event) {
+    }
 }
